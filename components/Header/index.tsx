be44a6811bdd5a93a964db903619364727902325
Imran Khan <emmi_khan2012@yahoo.com>
2026-08-24 11:29:10 +0200

    make header responsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,8 @@ import SubTitle from "../SubTitle"
 const Header = () => {
 
     return (
-        <header className="w-full px-4 py-6 sm:px-6 md:py-8 text-center">
+        <header className="flex flex-col text-center w-full max-w-3xl mx-auto mt-6 px-4 py-6 sm:px-6 md:py-8
+                rounded-2xl border border-[purple]">
             <Title  text = "Neon Reflex" />
             <SubTitle text = "Click the correct square"/>
         </header>
